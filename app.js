6a6decbffe7a709f7e7233e8f7803249d3b30a04
TariqKingCoder <tariqjjj111@gmail.com>
2025-12-02 00:33:14 +0430

    Update app.js

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ http.on('close', () => {
 });
 
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
 server.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
